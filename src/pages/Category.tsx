011adef0f1ab10b66840e782d19b98c1e3d53995
Mahdi Bagheri <qarjis8@gmail.com>
2023-03-25 18:46:31 +0430

    added Search & Category components in pages folder

---
+++ src/pages/Category.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+// Icons
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+
+const Category = () => {
+  return (
+    <div className="bg-slate-100 pb-10">
+      <div className="w-full bg-white h-10 flex items-center justify-center border-b border-slate-200 sticky top-0">
+        <label htmlFor="" className="text-sm">همه دسته ها</label>
+      </div>
+      <ul className="bg-white">
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">موبایل و کالای دیجیتال</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">لپ تاپ, کامپیوتر, اداری</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">هایپر مارکت</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">لوازم خانگی</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">مد و پوشاک</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">زیبایی و بهداشت</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">صوتی و تصویری</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">خودرو و سایر وسایل نقلیه</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">ورزش و سرگرمی</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">سلامت و پزشکی</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">فرهنگی هنری</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">کودک و نوزاد</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">ابزار و ساختمان</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">ابزارآلات</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">لوازم سفر و گردشگری</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">ارز و طلا</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">تجهیزات نگهداری و پرورش حیوانات</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">متفرقه</li>
+        </div>
+        <div className="flex items-center justify-between h-12 border-b border-slate-100 px-3">
+          <li className="pr-2 text-sm">تجهیزات صنعتی</li>
+          <NavigateBeforeIcon className="text-slate-500" />
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/pages/Search.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+// Components
+import Navbar from "../components/Navbar";
+import Banner from "../components/Banner";
+import AppleProducts from "../components/AppleProducts";
+import Banner2 from "../components/Banner2";
+import SamProducts from "../components/SamProducts";
+import Banner3 from "../components/Banner3";
+import XiaomiProducts from "../components/XiaomiProducts";
+import Banner4 from "../components/Banner4";
+import Banner5 from "../components/Banner5";
+import Banner6 from "../components/Banner6";
+import Banner7 from "../components/Banner7";
+
+const Search = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <AppleProducts />
+      <Banner2 />
+      <SamProducts />
+      <Banner3 />
+      <XiaomiProducts />
+      <Banner4 />
+      <AppleProducts />
+      <Banner5 />
+      <SamProducts />
+      <Banner6 />
+      <XiaomiProducts />
+      <Banner7 />
+    </div>
+  );
+};
+
+export default Search;
