846e02a42338b75544b0ef93bbb61b860e41359c
Mahdi Bagheri <qarjis8@gmail.com>
2023-06-08 14:57:55 +0330

    added sort method

--- src/components/AllProducts.tsx
+++ src/components/AllProducts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Products
@@ -12,10 +12,50 @@ import ClearIcon from "@mui/icons-material/Clear";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
+// interface
+interface ProductsType {
+  id: number;
+  image: string | any;
+  title: string;
+  storage: number;
+  minPrice: number;
+  maxPrice: number;
+}
+[];
+
 const AllProducts: React.FC = () => {
   const navigate = useNavigate();
 
   const data = [...apple, ...samsung, ...xiaomi];
+
+  const [isSort, setIsSort] = useState<boolean>(false);
+
+  // sort list display methods
+  const selectSort = () => {
+    document.getElementById("sort")!.classList.remove("hidden");
+  };
+  const cancelSelectSort = () => {
+    document.getElementById("sort")!.classList.add("hidden");
+  };
+
+  // Sort
+  let sort: string = "";
+  const selectedSort = (e: any) => {
+    sort = e.target.dataset.selectedSort;
+  };
+  let sortedProducts: ProductsType[] = [];
+  const sortProducts = () => {
+    console.log(sort);
+    sortedProducts = data.sort((a, b): any => {
+      if (sort === "Ascending") {
+        return a.minPrice > b.minPrice ? 1 : -1;
+      } else if (sort === "Descending") {
+        return a.minPrice < b.minPrice ? 1 : -1;
+      }
+    });
+    setIsSort(true);
+    cancelSelectSort();
+  };
 
   return (
     <div className="bg-slate-100">
@@ -47,7 +87,10 @@ const AllProducts: React.FC = () => {
             <p className="text-sm">برند</p>
             <ExpandMoreIcon fontSize="small" className="scale-75" />
           </div>
-          <div className="flex items-center justify-center flex-shrink-0">
+          <div
+            onClick={selectSort}
+            className="flex items-center justify-center flex-shrink-0"
+          >
             <p className="text-sm">مرتب سازی</p>
             <ExpandMoreIcon fontSize="small" className="scale-75" />
           </div>
@@ -95,7 +138,7 @@ const AllProducts: React.FC = () => {
       </div>
       {/* products */}
       <div className="mx-3 flex items-center justify-between flex-wrap gap-2 pb-2">
-        {data.map((product) => (
+        {(isSort ? sortedProducts : data).map((product) => (
           <div key={product.id} className="bg-white rounded px-3 w-40 h-80">
             <img
               src={Object.values(product.image)[0]! as string}
@@ -113,6 +156,43 @@ const AllProducts: React.FC = () => {
             </div>
           </div>
         ))}
+      </div>
+      {/* Sort */}
+      <div id="sort" className="bg-white z-20 w-full h-full fixed top-0 hidden">
+        <div className="flex items-center border-b border-slate-100 py-2 px-3">
+          <ClearIcon onClick={cancelSelectSort} fontSize="small" />
+          <h3 className="mx-auto text-xs">انتخاب مرتب سازی</h3>
+        </div>
+        <div className="flex flex-col items-start gap-3 px-5 py-3">
+          <button
+            className="text-sm hover:text-red-600"
+            data-selected-sort="Ascending"
+            onClick={selectedSort}
+          >
+            ارزان ترین
+          </button>
+          <button
+            className="text-sm hover:text-red-600"
+            data-selected-sort="Descending"
+            onClick={selectedSort}
+          >
+            گران ترین
+          </button>
+        </div>
+        <div
+          className="flex items-center justify-between gap-3 py-2 px-3 
+              border-t border-slate-100 absolute bottom-0 w-full"
+        >
+          <button
+            onClick={sortProducts}
+            className="text-sm text-white bg-zinc-700 w-full py-1 rounded"
+          >
+            اعمال فیلتر
+          </button>
+          <button className="text-sm text-zinc-700 px-3 py-1 border border-zinc-700 rounded">
+            حذف
+          </button>
+        </div>
       </div>
     </div>
   );
